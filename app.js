f665c848b054e2b93f487510ae506ff524223ee3
amaugosomto <amaugosomto@gmail.com>
2022-07-12 23:02:54 +0200

    added config files, env files and mongodb

--- app.js
+++ app.js
@@ -2,12 +2,23 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var mongoose = require("mongoose");
+var helmet = require('helmet');
+var cors = require("cors");
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
 
+const mongoUrl =  require("./config").mongo_url;
+mongoose
+    .connect(mongoUrl, { useNewUrlParser: true })
+    .then(() => console.log("Database Connected successfully"))
+    .catch(err => console.log(err));
+
+app.use(cors());
+app.use(helmet());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
---
+++ config/env-dev.js
@@ -0,0 +1,5 @@
+require("dotenv").config();
+
+module.exports = {
+  mongo_url: process.env.MONGODB_URI
+};
